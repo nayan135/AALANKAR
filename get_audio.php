66611009ff76eb75512559b5ffca2aed5efb46f6
NAYAN <acharyanayan604@gmail.com>
2024-02-26 20:35:57 +0545

    Update get_audio.php and add image and audio files

--- get_audio.php
+++ get_audio.php
@@ -23,7 +23,7 @@ $result = $stmt->get_result();
     <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-  <!--  <lonk rel="stylesheet/less" type="text/css" href="new.less">-->
+  <!--  <link rel="stylesheet/less" type="text/css" href="new.less">-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 </head>  
